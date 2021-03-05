e04bf1e16c3c59d8a88ad4e88b64f71687c1997f
amita <msamita@hotmail.com>
2021-03-05 11:26:45 -0500

    added first version

---
+++ src/main/java/com/l3amita/bdd/bddlogin/BddloginApplication.java
@@ -0,0 +1,13 @@
+package com.l3amita.bdd.bddlogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddloginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/l3amita/bdd/bddlogin/BddloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.l3amita.bdd.bddlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BddloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
